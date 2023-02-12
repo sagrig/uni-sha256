50c83dc23714efc9a6ed5a7b0217197c55917804
Sahak Ivasauskas <sahak.ivasauskas@mif.stud.vu.lt>
2023-02-12 15:38:55 +0200

    update: unisha256.h

--- unisha256.h
+++ unisha256.h
@@ -1,13 +1,16 @@
 #ifndef UNISHA256_H
 #define UNISHA256_H
 
-struct sha2_blck {
-     uint32_t blk_word[16];
+#define WSCHEDLNUM 64U
+
+struct sha2_chnk {
+     uint32_t chnk_word[WSCHEDLNUM];
 };
 
 struct usha_ctx {
-     struct sha2_blck *ctx_blks;
-     uint64_t          ctx_blen;
+     struct sha2_chnk *ctx_chnk;
+     uint64_t          ctx_clen;
+     bool              ctx_fret;
 };
 
 #endif /* UNISHA256_H */
