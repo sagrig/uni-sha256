14d1e29758d9a1f4d22c56e3672e8163bd53f672
Sahak Ivasauskas <sahak.ivasauskas@mif.stud.vu.lt>
2023-02-16 01:12:34 +0200

    update: sha2bits.h

--- sha2bits.h
+++ sha2bits.h
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
+#include "unisha256.h"
+
 static inline uint32_t bits_rotr(const uint32_t inpt, const uint8_t rotv)
 {
      return (inpt >> rotv)|(inpt << (32 - rotv));
@@ -13,4 +15,27 @@ static inline uint32_t sigm_fun0(const uint32_t inpt)
 static inline uint32_t sigm_fun1(const uint32_t inpt)
 {
      return bits_rotr(inpt, 17) ^ bits_rotr(inpt, 19) ^ (inpt >> 10);
+}
+
+static inline uint32_t summ_fun0(const uint32_t inpt)
+{
+     return bits_rotr(inpt, 2) ^ bits_rotr(inpt, 13) ^ bits_rotr(inpt, 22);
+}
+
+static inline uint32_t summ_fun1(const uint32_t inpt)
+{
+     return bits_rotr(inpt, 6) ^ bits_rotr(inpt, 11) ^ bits_rotr(inpt, 25);
+}
+
+static inline uint32_t choi_func(const struct usha_ctx *ctx)
+{
+     return (ctx->ctx_hash[4] & ctx->ctx_hash[5]) ^
+	   (~ctx->ctx_hash[4] & ctx->ctx_hash[6]);
+}
+
+static inline uint32_t majr_func(const struct usha_ctx *ctx)
+{
+     return (ctx->ctx_hash[0] & ctx->ctx_hash[1]) ^
+	  (ctx->ctx_hash[0] & ctx->ctx_hash[2]) ^
+	  (ctx->ctx_hash[1] & ctx->ctx_hash[2]);
 }
