0619a160a8f68f44828c1cb76a4fab708551c677
Sahak Ivasauskas <sahak.ivasauskas@mif.stud.vu.lt>
2023-02-16 15:50:58 +0200

    update: main.c

--- main.c
+++ main.c
@@ -93,6 +93,10 @@ eror:
 static void chnk_work(struct usha_ctx *ctx)
 {
      uint32_t *wsch = NULL;
+     uint32_t tmp1 = 0;
+     uint32_t tmp2 = 0;
+     uint32_t work[8];
+
      for (uint32_t i = 0; i < ctx->ctx_clen; ++i) {
 	  wsch = (uint32_t *)((struct sha2_chnk *)ctx->ctx_chnk + i);
 
@@ -106,7 +110,48 @@ static void chnk_work(struct usha_ctx *ctx)
 	       wsch[j] += sigm_fun0(wsch[j-15]);
 	       wsch[j] += sigm_fun1(wsch[j-2]);
 	  }
+
+	  for (uint8_t j = 0; j < 8; ++j) {
+	       work[j] = ctx->ctx_hash[j];
+	  }
+
+	  for (uint8_t j = 0; j < WSCHEDLNUM; ++j) {
+	       tmp1  = work[7]; // [h]
+	       tmp1 += summ_fun1(work[4]); // pass [e]
+	       tmp1 += choi_func(work);
+	       tmp1 += kval[j];
+	       tmp1 += wsch[j];
+
+	       tmp2  = summ_fun0(work[0]); // pass [a]
+	       tmp2 += majr_func(work);
+
+	       work[7] = work[6];        // [h] = [g]
+	       work[6] = work[5];        // [g] = [f]
+	       work[5] = work[4];        // [f] = [e]
+	       work[4] = work[3] + tmp1; // [e] = [d] + [t1]
+	       work[3] = work[2];        // [d] = [c]
+	       work[2] = work[1];        // [c] = [b]
+	       work[1] = work[0];        // [b] = [a]
+	       work[0] = tmp1 + tmp2;             // [a] = [t1] + [t2]
+	  }
+
+	  for (uint8_t j = 0; j < 8; ++j) {
+	       ctx->ctx_hash[j] += work[j];
+	       printf("%u: %u\n", j, ctx->ctx_hash[j]);
+	  }
      }
+}
+
+static void hash_init(struct usha_ctx *ctx)
+{
+     ctx->ctx_hash[0] = 0x6a09e667;
+     ctx->ctx_hash[1] = 0xbb67ae85;
+     ctx->ctx_hash[2] = 0x3c6ef372;
+     ctx->ctx_hash[3] = 0xa54ff53a;
+     ctx->ctx_hash[4] = 0x510e527f;
+     ctx->ctx_hash[5] = 0x9b05688c;
+     ctx->ctx_hash[6] = 0x1f83d9ab;
+     ctx->ctx_hash[7] = 0x5be0cd19;
 }
 
 int main(void)
@@ -121,6 +166,7 @@ int main(void)
      if (errl = __LINE__, false == ushactx.ctx_fret)
 	  goto eror;
 
+     hash_init(&ushactx);
      chnk_work(&ushactx);
 
      prnt_bits(ushactx.ctx_chnk, ushactx.ctx_clen * sizeof *ushactx.ctx_chnk);
